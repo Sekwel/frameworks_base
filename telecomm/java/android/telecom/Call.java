fc5e31b9d9d35788eff7fffbac242b0cb516352a
Mike Wielgosz <mike@sekwel.com>
2016-06-21 14:27:53 -0400

    telecom/Call: Fix compile error

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -240,12 +240,6 @@ public final class Call {
         // Next PROPERTY value: 0x00000020
         //******************************************************************************************
 
-        /**
-         * Speed up audio setup for MT call.
-         * @hide
-         */
-        public static final int CAPABILITY_SPEED_UP_MT_AUDIO = 0x00008000;
-
         private final Uri mHandle;
         private final int mHandlePresentation;
         private final String mCallerDisplayName;
@@ -387,9 +381,6 @@ public final class Call {
             }
             if (hasProperty(properties, PROPERTY_EMERGENCY_CALLBACK_MODE)) {
                 builder.append(" PROPERTY_EMERGENCY_CALLBACK_MODE");
-            }
-            if (can(capabilities, CAPABILITY_SPEED_UP_MT_AUDIO)) {
-                builder.append(" CAPABILITY_SPEED_UP_IMS_MT_AUDIO");
             }
             builder.append("]");
             return builder.toString();
